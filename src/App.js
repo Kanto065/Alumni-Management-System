12c83c1fa71141856a04c9b6d4f4ea31ac59ae08
Kanta Bhattacharjee <kanto.uv@outlook.com>
2023-03-12 18:50:30 +0600

    navbar created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './Components/Home/Home';
+import Header from './Components/Common/Header/Header';
+
 
 function App() {
   return (
     <div>
+      <Header></Header>
       <Home></Home>
     </div>
   );
---
+++ src/Components/Common/Header/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import "./Header.css";
+
+const Header = () => {
+    return (
+        <nav className='header'>
+            <div>
+                <a href="">Home</a>
+                <a href="">Gallery</a>
+                <a href="">Alumni List</a>
+                <a href="">Jobs</a>
+                <a href="">Forum</a>
+                <a href="">Profile</a>
+            </div>
+        </nav>
+
+    );
+};
+
+export default Header;
